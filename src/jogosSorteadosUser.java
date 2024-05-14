a9073b3772c44d3da7aebfd88c9c921a824ca2c0
Seishiro2 <athostosenai@gmail.com>
2024-05-14 11:07:02 -0300

    acabei de criar as telas, tentando configura-las

--- src/jogosSorteadosUser.java
+++ src/jogosSorteadosUser.java
@@ -1,2 +1,16 @@
-public class jogosSorteadosUser {
+import javax.swing.*;
+import java.awt.*;
+
+public class jogosSorteadosUser extends JFrame {
+    private JPanel painelSorteados;
+    private JLabel label22;
+
+    public jogosSorteadosUser(){
+
+        setSize(900,600);
+        setContentPane(painelSorteados);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+
+    }
 }
